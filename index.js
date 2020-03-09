21846f05d50dc8b6dc7de33a80deeafa2a8f4fa2
Nikita Savelev <nsavelev@yahoo.com>
2020-03-09 18:46:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function appendKitten(name){
 return kittens.concat(name)  
 }
 function prependKitten(name){
-return kittens.concat(name)
+return name.concat(kittens)
 }
 function removeLastKitten(){
   
