6977e16650ed2129f1358be4c1a94424bc82a440
Nikita Savelev <nsavelev@yahoo.com>
2020-03-09 19:24:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function prependKitten(name){
 return [name,...kittens]
 }
 function removeLastKitten(){
-  return kittens.slice(2)
+  return kittens.slice(0,1)
 }
 function removeFirstKitten(){
   return kittens.slice(0,1)}
